802ed9b194e888b5c01c57b7d0e8d93d6b7b98aa
yudha alfian <yudhanurulalfian@gmail.com>
2022-11-10 14:37:28 +0700

    main file - modularisasi dengan function

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 """
 Aplikasi deteksi gempa terkini
+MODULARISASI DENGAN FUNCTION
 """
+
+def ekstraksi_data():
+    pass
+
+def tampilkan_data(result):
+    pass
 
 if __name__ == '__main__':
     print('aplikasi utama')
+    result = ekstraksi_data()
+    tampilkan_data(result)
+
 
