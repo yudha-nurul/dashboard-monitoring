00fcfa3c209966b4b003c066788f8fc649a4f5cc
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-11-10 05:53:36 -0800

    main. aplikasi gempa terkini

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ MODULARISASI DENGAN FUNCTION
 """
 
 def ekstraksi_data():
+    """
+        Gempabumi Terkini
+    Tanggal: 10 November 2022
+    Waktu: 12:56:30 WIB
+    Magnitudo: 4.9
+    Kedalaman: 10 km
+    Lokasi: 4.28 LU 96.72 BT
+    Pusat Gempa: Pusat gempa berada di darat 40 km BaratDaya Takengon
+    Dirasakan: Dirasakan (Skala MMI): III Nagan Raya, III Benermeriah, I-II Pidie, I-II Lhokseumawe
+    :return:
+    """
     pass
 
 def tampilkan_data(result):
