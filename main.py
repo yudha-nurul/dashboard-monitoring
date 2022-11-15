51f68ab1e7a03f3d543b1203541cbe262100ceb7
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-11-15 07:28:16 -0800

    modularisasi dengan package

--- main.py
+++ main.py
@@ -2,9 +2,9 @@
 Aplikasi deteksi gempa terkini
 MODULARISASI DENGAN FUNCTION
 """
-from gempaterkini import ekstraksi_data, tampilkan_data
+import gempaterkini
 
 if __name__ == '__main__':
     print('aplikasi utama')
-    result = ekstraksi_data()
-    tampilkan_data(result)
+    result = gempaterkini.ekstraksi_data()
+    gempaterkini.tampilkan_data(result)
