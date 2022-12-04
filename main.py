61311ecce87efe374ac0a3607f5626ac3e54d051
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-12-04 21:55:14 +0800

    gempa terkini latihan ok

--- main.py
+++ main.py
@@ -8,9 +8,12 @@ import latihan_gempaterkini
 if __name__ == '__main__':
     print('aplikasi utama')
     print('-' * 40)
+    # result = gempaterkini.ekstraksi_data()
     result = latihan_gempaterkini.ekstraksi_data()
     if result is None:
         print('program berhenti')
     else:
+        # gempaterkini.tampilkan_data(result)
         latihan_gempaterkini.tampilkan_data(result)
+        # latihan_hotnews_detik.tampilkan_data(result)
     print('-' * 40)
