e29ac71f342cc10d5355d3d6e4f87545aa3deda2
yudha nurul alfian <yudhanurulalfian@gmail.com>
2022-12-05 01:50:49 +0800

    gempa terkini & latihan gempa terkini sudah ok

--- main.py
+++ main.py
@@ -3,17 +3,18 @@ Aplikasi deteksi gempa terkini
 MODULARISASI DENGAN FUNCTION
 MODULARISASI DENGAN PACKAGE
 """
-import latihan_gempaterkini
+# import latihan_gempaterkini
+import gempaterkini
 
 if __name__ == '__main__':
     print('aplikasi utama')
     print('-' * 40)
-    # result = gempaterkini.ekstraksi_data()
-    result = latihan_gempaterkini.ekstraksi_data()
+    result = gempaterkini.ekstraksi_data()
+    # result = latihan_gempaterkini.ekstraksi_data()
     if result is None:
         print('program berhenti')
     else:
-        # gempaterkini.tampilkan_data(result)
-        latihan_gempaterkini.tampilkan_data(result)
+        gempaterkini.tampilkan_data(result)
+        # latihan_gempaterkini.tampilkan_data(result)
         # latihan_hotnews_detik.tampilkan_data(result)
     print('-' * 40)
